49b0e697df434e0309c393055fec568d5639271f
Maria <maria.villaloboschaves@gmail.com>
2024-03-12 19:14:05 -0600

    Api Key set in local storage

--- src/components/ApiKeyModal.js
+++ src/components/ApiKeyModal.js
@@ -1,6 +1,4 @@
-// import { getApiKey } from "../lib/ApiKey";
-
-// import { setApiKey } from "../lib/ApiKey";
+import { getApiKey, setApiKey } from "../lib/ApiKey.js";
 
 export const ApiKeyModal = () => {
   const apiKeyContainer = document.createElement("div");
@@ -8,24 +6,24 @@ export const ApiKeyModal = () => {
 
   const inputApiKey = document.createElement("input");
   inputApiKey.classList.add("input-apikey");
-  inputApiKey.setAttribute("type", "hidden");
+  inputApiKey.setAttribute("type", "text");
 //   inputApiKey.placeholder("Ingrese su Api Key");
-  inputApiKey.value = "el api key que esta en el localstorage ";
+//   inputApiKey.value = "el api key que esta en el localstorage ";
 
 
-  const textInput = document.createElement("p");
-  textInput.classList.add("text-apikey");
-  textInput.innerHTML = "Ingrese su Api Key: ";
+//   const textInput = document.createElement("p");
+//   textInput.classList.add("text-apikey");
+//   textInput.innerHTML = "Ingrese su Api Key: ";
 
   const buttonSaveApiKey = document.createElement("button");
-  buttonSaveApiKey.setAttribute("data-testid", "button-ApiKey");
+  buttonSaveApiKey.setAttribute("data-testid", "button-apiKey");
+  buttonSaveApiKey.classList.add("apiKey-button")
   buttonSaveApiKey.innerHTML = "Save";
-//   buttonSaveApiKey.addEventListener("click", function(){
-//     const catchApiKey = inputApiKey.querySelector('input[class="input-apikey"]').value;
-//     const apiKeySave = setApiKey(getApiKey(), catchApiKey);
-//     return apiKeySave;
-//   });
+  buttonSaveApiKey.addEventListener("click", function(){
+    const myinput = document.querySelector('input[class="input-apikey"]')
+    setApiKey(myinput.value);
+  });
 
-  apiKeyContainer.append(textInput, inputApiKey, buttonSaveApiKey);
+  apiKeyContainer.append(inputApiKey, buttonSaveApiKey);
   return apiKeyContainer;
 };
--- src/lib/apiKey.js
+++ src/lib/apiKey.js
@@ -1,11 +1,10 @@
 export const getApiKey = () => {
   // Implementa el código para obtener la API KEY desde Local Storage
-  const apiKeyStorage = localStorage.getItem("api-key");
+  const apiKeyStorage = window.localStorage.getItem("api-key");
   return apiKeyStorage;
 };
 
 export const setApiKey = (key) => {
   // Implementa el código para guardar la API KEY en Local Storage
-  const apiKeySave = localStorage.setItem("api-key", key)
-  return apiKeySave;
+  const apiKeySave = localStorage.setItem("api-key", key);
 };
