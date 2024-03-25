bd0b23b4a6b74c25f9bdb55dde325492ba963b76
Maria <maria.villaloboschaves@gmail.com>
2024-03-24 22:55:29 -0600

    Menu,spec testing iconHome calls navigateTo

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -11,26 +11,7 @@ export const iconHome = () => {
   buttonHome.addEventListener("click", () => navigateTo("/", {}));
   return buttonHome;
 };
-// export const indChat = () => {
-//   const indIconChats = document.createElement("div");
-//   indIconChats.classList.add("ind-chat");
-//   const btnIndChat = document.createElement("button");
-//   btnIndChat.setAttribute("data-testid", "btn-ind-chat");
-//   btnIndChat.innerHTML = "Individual Chat";
-//   indIconChats.append(btnIndChat);
-//   btnIndChat.addEventListener("click", () => navigateTo("/individualChat", {}));
-//   return indIconChats;
-// };
-// export const grpChat = () => {
-//   const groupIconChats = document.createElement("div");
-//   groupIconChats.classList.add("grp-chat");
-//   const btnGrpChat = document.createElement("button");
-//   btnGrpChat.setAttribute("data-testid", "btn-grp-chats");
 
-//   groupIconChats.append(btnGrpChat);
-//   btnGrpChat.addEventListener("click", () => navigateTo("/groupChat", {}));
-//   return groupIconChats;
-// };
 export const iconGrpChat = () => {
   const divIconChats = document.createElement("div");
   divIconChats.classList.add("chats");
@@ -52,18 +33,7 @@ export const iconGrpChat = () => {
     boxApiKey.append(closeButton);
 
     closeButton.addEventListener("click", function closePopup() {
-      // const headerTitle = document.querySelector(".headerTittle");
-      // headerTitle.style.visibility = "visible";
-      // const funcStats = document.querySelector(
-      //   'div[class = "functionsContainer"]'
-      // );
-      // const itemStats = document.querySelector(
-      //   'div[class = "items-container"]'
-      // );
-      // funcStats.style.filter = "none";
-      // itemStats.style.filter = "none";
       grpChatWind.removeChild(boxApiKey);
-      // divIconChats.removeChild(closeButton);
     });
   }
   buttonChats.addEventListener("click", async () => {
@@ -78,54 +48,6 @@ export const iconGrpChat = () => {
   divIconChats.appendChild(buttonChats);
   return divIconChats;
 };
-// export const iconChats = () => {
-//   const divIconChats = document.createElement("div");
-//   divIconChats.classList.add("grp-chats");
-//   const buttonChats = document.createElement("button");
-//   buttonChats.setAttribute("data-testid", "button-chats");
-//   buttonChats.innerHTML = "Chats";
-
-//   buttonChats.addEventListener("click", function popupApiKey() {
-//     const headerTitle = document.querySelector(".headerTittle");
-//     const funcStats = document.querySelector('div[class = "functionsContainer"]');
-//     const itemStats = document.querySelector('div[class = "items-container"]');
-//     funcStats.style.filter = "blur(3px)";
-//     itemStats.style.filter = "blur(3px)";
-//     headerTitle.style.visibility = "hidden";
-//     const boxApiKey = document.createElement("div");
-//     boxApiKey.classList.add("boxApiKey");
-//     const apiKeyPopup = ApiKeyModal();
-
-//     const closeButton = document.createElement("button");
-//     closeButton.className = "buttonCloseApiModal";
-//     closeButton.textContent = "Close";
-
-//     divIconChats.append(boxApiKey);
-//     boxApiKey.appendChild(apiKeyPopup);
-
-//     closeButton.addEventListener("click", function closePopup() {
-//       const headerTitle = document.querySelector(".headerTittle");
-//       headerTitle.style.visibility = "visible";
-//       const funcStats = document.querySelector(
-//         'div[class = "functionsContainer"]'
-//       );
-//       const itemStats = document.querySelector(
-//         'div[class = "items-container"]'
-//       );
-//       funcStats.style.filter = "none";
-//       itemStats.style.filter = "none";
-//       divIconChats.removeChild(boxApiKey);
-//       // divIconChats.removeChild(closeButton);
-//       console.log(divIconChats);
-//       console.log(headerTitle);
-//     });
-
-//     boxApiKey.appendChild(closeButton);
-//   });
-
-//   divIconChats.appendChild(buttonChats);
-//   return divIconChats;
-// };
 
 export const iconStatistics = () => {
   const divStatistics = document.createElement("div");
--- test/Menu.spec.js
+++ test/Menu.spec.js
@@ -1,30 +1,7 @@
-import { iconHome } from "../src/components/Menu";
+import { iconHome } from "../src/components/Menu.js";
+import { navigateTo } from "../src/router.js";
 
-// fdescribe("iconHome", () => {
-//   test("it should be a button", () => {
-//     const mockContainer = document.createElement("div");
-//     // document.body.appendChild(mockContainer);
-
-//     mockContainer.append(iconHome());
-
-//     const divElement = mockContainer.querySelector('div');
-
-//     // Assert that the div exists
-//     expect(divElement).toBeDefined();
-
-//     // Assert that the button exists inside the div
-//     const buttonElement = divElement.querySelector('button');
-//     expect(buttonElement).toBeDefined();
-
-//     // Assert that the button has the correct class
-//     expect(buttonElement.classList.contains('tittleButton')).toBe(true);
-
-//     // Assert that the button has the correct text content
-//     expect(buttonElement.textContent).toBe('ARCHITOPIA');
-
-//   });
-// });
-
+jest.mock("../src/router.js");
 //Test if the button exists.
 
 describe('iconHome', () => {
@@ -35,17 +12,29 @@ describe('iconHome', () => {
   //Then, test if the button has the correct class.
 
   test('it should have a class tag', () => {
-    const iconButton = document.createElement('button');
-    iconButton.classList.add('tittleButton');
-    iconButton.innerHTML = "ARCHITOPIA";
-    expect(iconHome()).toStrictEqual(iconButton);
+    // const iconButton = document.createElement('button');
+    // iconButton.classList.add('tittleButton');
+    // iconButton.innerHTML = "ARCHITOPIA";
+    const iconButton = iconHome();
+    expect(iconButton.tagName).toBe('BUTTON')
+    expect(iconButton.classList.contains('tittleButton')).toBe(true);
   });
-  // Then, test if iconHome calls navigateTo
 
-  //   test("when an user clicks iconHome, it has to call navigateTo", () => {
-  //     const 
-  //     const displayNavigateTo =  iconButton.addEventListener("click", () => navigateTo("/", {}));
-  //     expect(iconHome()).
-  //   })
+  // Then, test if iconHome calls navigateTo when a click happens
+
+  test("when an user clicks iconHome, it has to call navigateTo", () => {
+    //Here, it has to mock navigateTo function
+
+    navigateTo.mockImplementation(() => {});
+    
+    //Renderiza button Home & have access to the button
+    //     const iconButton =  iconHome();
+    //     const selectedIconHome = iconButton.querySelector('.tittleButton');
+    
+    //     //Simulate a click
+    // //     selectedIconHome.click();
+    
+    expect(navigateTo()).toHaveBeenCalledWith('/', {});
+  });
 
 })
